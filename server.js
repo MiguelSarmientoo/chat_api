b878979d0f069b123af6f19c96f8226f637a1ff3
XxDavexX <dave.santivanez@gmail.com>
2024-07-17 12:25:57 -0500

    avanceDave

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors'); 
 const messagesRouter = require('./routes/messages');
 const usersRouter = require('./routes/user');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(cors());
 
 app.use(bodyParser.json());
 app.use('/api', messagesRouter);
